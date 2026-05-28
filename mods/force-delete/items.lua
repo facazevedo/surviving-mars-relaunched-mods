2951c56025e3298cdfd86597244cec2400ecb5b4
Frederico A. C. Azevedo <fredericoacazevedo@gmail.com>
2026-05-28 14:28:50 -0400

    Rename force delete code modules to snake_case

--- mods/force-delete/items.lua
+++ mods/force-delete/items.lua
@@ -4,63 +4,63 @@ return {
 		'CodeFileName', "Code/ForceDelete.lua",
 	}),
 	PlaceObj('ModItemCode', {
-		'name', "ForceDeleteConfig",
+		'name', "fd_config",
 		'CodeFileName', "Code/fd_config.lua",
 	}),
 	PlaceObj('ModItemCode', {
-		'name', "ForceDeleteDisplayAttributes",
+		'name', "fd_display_attributes",
 		'CodeFileName', "Code/fd_display_attributes.lua",
 	}),
 	PlaceObj('ModItemCode', {
-		'name', "ForceDeleteColonist",
+		'name', "fd_colonist",
 		'CodeFileName', "Code/fd_colonist.lua",
 	}),
 	PlaceObj('ModItemCode', {
-		'name', "ForceDeleteDrone",
+		'name', "fd_drone",
 		'CodeFileName', "Code/fd_drone.lua",
 	}),
 	PlaceObj('ModItemCode', {
-		'name', "ForceDeleteAnimal",
+		'name', "fd_animal",
 		'CodeFileName', "Code/fd_animal.lua",
 	}),
 	PlaceObj('ModItemCode', {
-		'name', "ForceDeleteShuttle",
+		'name', "fd_shuttle",
 		'CodeFileName', "Code/fd_shuttle.lua",
 	}),
 	PlaceObj('ModItemCode', {
-		'name', "ForceDeleteRover",
+		'name', "fd_rover",
 		'CodeFileName', "Code/fd_rover.lua",
 	}),
 	PlaceObj('ModItemCode', {
-		'name', "ForceDeleteTrain",
+		'name', "fd_train",
 		'CodeFileName', "Code/fd_train.lua",
 	}),
 	PlaceObj('ModItemCode', {
-		'name', "ForceDeleteRocket",
+		'name', "fd_rocket",
 		'CodeFileName', "Code/fd_rocket.lua",
 	}),
 	PlaceObj('ModItemCode', {
-		'name', "ForceDeleteDeposit",
+		'name', "fd_deposit",
 		'CodeFileName', "Code/fd_deposit.lua",
 	}),
 	PlaceObj('ModItemCode', {
-		'name', "ForceDeleteDecoration",
+		'name', "fd_decoration",
 		'CodeFileName', "Code/fd_decoration.lua",
 	}),
 	PlaceObj('ModItemCode', {
-		'name', "ForceDeleteInfrastructure",
+		'name', "fd_infrastructure",
 		'CodeFileName', "Code/fd_infrastructure.lua",
 	}),
 	PlaceObj('ModItemCode', {
-		'name', "ForceDeleteInternalBuilding",
+		'name', "fd_internal_building",
 		'CodeFileName', "Code/fd_internal_building.lua",
 	}),
 	PlaceObj('ModItemCode', {
-		'name', "ForceDeleteExternalBuilding",
+		'name', "fd_external_building",
 		'CodeFileName', "Code/fd_external_building.lua",
 	}),
 	PlaceObj('ModItemCode', {
-		'name', "ForceDeleteDome",
+		'name', "fd_dome",
 		'CodeFileName', "Code/fd_dome.lua",
 	}),
 }
